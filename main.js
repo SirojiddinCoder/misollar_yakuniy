3fab080f6be5eeac1f6a426fe9405d1489c2862f
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-28 11:10:12 -0800

    optimation

--- main.js
+++ main.js
@@ -20,37 +20,56 @@
 // }
 // findNum(81);
 
-let s=0;
-    str1="ABC";
-    str2="BCA";
-    for (let i = 0; i < srt1.length; i++) {
-        if (!(str2.includes(str1[i]))) {
-            s++;
-        }
-    }
-    if (s>0) {
-        console.log(false)
-    }
-    else{
-        console.log(true);
-    }
-const myfunc = (arr, num) => {
-    let sum = 0;
-    let k = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arr.length; j++) {
-        if(arr[i] % num == 0){
-            sum += arr[i];
-            console.log(sum);
-            k++;
-        }else{
-            sum += arr[j] ;
-            (sum / (j+1)) == num && k++;
-            console.log(sum);
-        }
-    }
-  }
-  console.log(k);
-};
+// let s=0;
+//     str1="ABC";
+//     str2="BCA";
+//     for (let i = 0; i < srt1.length; i++) {
+//         if (!(str2.includes(str1[i]))) {
+//             s++;
+//         }
+//     }
+//     if (s>0) {
+//         console.log(false)
+//     }
+//     else{
+//         console.log(true);
+//     }
+// const myfunc = (arr, num) => {
+//     let sum = 0;
+//     let k = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 1; j < arr.length; j++) {
+//         if(arr[i] % num == 0){
+//             sum += arr[i];
+//             console.log(sum);
+//             k++;
+//         }else{
+//             sum += arr[j] ;
+//             (sum / (j+1)) == num && k++;
+//             console.log(sum);
+//         }
+//     }
+//   }
+//   console.log(k);
+// };
 
-myfunc([1,3,2,2],2);
+// myfunc([1,3,2,2],2);
+
+function getData(arr, num) {
+    let count = 0;
+    let sum = 0;
+
+    for (i = 0; i < arr.length; i++) {
+        for (j = i; j < arr.length; j++) {
+            sum += arr[j]
+
+            if (sum / (j - i + 1) == num) {
+                count++;
+                console.log(sum,  (j - i + 1));
+            }
+        }
+        sum = 0;
+    }
+    console.log(count);
+}
+getData([1, 3, 2, 2], 2);
